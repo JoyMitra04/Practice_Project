29bc4ca8b8ca314974d253540296d21434b87f7d
Joy MItra <joymitra523@gmail.com>
2023-03-29 22:27:20 +0600

    Added sir exam file

---
+++ Exam.py
@@ -0,0 +1,42 @@
+"""
+Problem-08:
+Provided a list where every element occurs doubled except for one. Find that single one.
+
+Problem-09:
+Some operators checks about the relationship between two values and these operators are called relational operators. Given two numerical values your job is just to find out the relationship between them
+that is (i) First one is greater than the second (ii) First one is less than the second or (iii) First and
+second one is equal.
+Input
+First line of the input file is an integer t (t < 15) which denotes how many sets of inputs are there.
+Each of the next t lines contain two integers a and b (|a|, |b| < 1000000001).
+Output
+For each line of input produce one line of output. This line contains any one of the relational operators
+‘>’, ‘<’ or ‘=’, which indicates the relation that is appropriate for the given two numbers.
+Sample Input
+3
+10 20
+20 10
+10 10
+
+
+Problem-10:
+10055 Hashmat the brave warrior
+
+Hashmat is a brave warrior who with his group of young soldiers moves from one place to another to
+fight against his opponents. Before Fighting he just calculates one thing, the difference between his
+soldier number and the opponent’s soldier number. From this difference he decides whether to fight or
+not. Hashmat’s soldier number is never greater than his opponent.
+Input
+The input contains two numbers in every line. These two numbers in each line denotes the number
+soldiers in Hashmat’s army and his opponent’s army or vice versa. The input numbers are not greater
+than 2
+32. Input is terminated by ‘End of File’.
+Output
+For each line of input, print the difference of number of soldiers between Hashmat’s army and his
+opponent’s army. Each output should be in seperate line.
+Sample Input
+10 12
+10 14
+100 200
+
+"""
